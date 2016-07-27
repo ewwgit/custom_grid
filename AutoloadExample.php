2f5a68de945c30b7f5cac559a9fde94c88640485
himabindu adi <himabindu adi>
2016-07-27 11:38:43 +0530

    initail commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace frontend\custom;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
